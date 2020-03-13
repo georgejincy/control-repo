061ff535b227349f7b76d81c6cb85f9dff019c0e
Jincy George <jincydon101@gmail.com>
2020-03-13 14:58:34 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDABuS2nPhKI5NC5z0CpaDNfT4DPETPsZ8MaaeJtsl7Uu5CGZ91gaMRu9iahMwT7sQkyRJwRZt8SjaKoOf1jNbo+bk1GNRBwVlytt1Amcox7+HdouLF3IjY9OTMhp0uvqAkfTPNmDzpPukyHFA3XD2pFZOJ8f0CCkiJcLdeeypnGMf01UGF81Yvz0q6/lVQycw4RybY4DhQW2SxEpzzvGU59irIqZ+IG6lOjT7dSGguQiG+rPXNWyeEjnoh74fXGXk+lld5A+CO88ku7HqI7Nf+ctQFjKlS5GjhVGd9zywjQr7tEAoe4PH1NHDJrFcA5/azT0D/+gC5PXS7RsBGac2L',
   }  
 }
